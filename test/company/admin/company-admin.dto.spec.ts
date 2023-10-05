d64d254d95be2e6b2b99e1a6c3c3197e1a085294
Marcos Soares <marcossaore@gmail.com>
2023-10-05 14:00:28 -0300

    test: Company Dto

---
+++ test/company/admin/company-admin.dto.spec.ts
@@ -0,0 +1,21 @@
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { CompanyAdminDto } from '../../../src/company/admin/company-admin.dto';
+
+describe('Dto: CompanyAdminDto', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+
+    it('should return all erros when data is no provided', async () => {
+        const validation = plainToInstance(CompanyAdminDto, { });
+        const errors = await validate(validation);
+        expect(errors[0].constraints.isNotEmpty).toEqual('{\"message\":\"O nome do admin deve ser informado!\",\"field\":\"name\"}');
+        expect(errors[0].constraints.isString).toEqual('{\"message\":\"O nome do admin deve ser do tipo \\\"string\\\"!\",\"field\":\"name\"}');
+        expect(errors[1].constraints.isNotEmpty).toEqual('{\"message\":\"O cpf do admin deve ser informado!\",\"field\":\"document\"}');
+        expect(errors[1].constraints.isCpf).toEqual('{\"message\":\"O CPF do admin não é valido!\",\"field\":\"document\"}');
+        expect(errors[2].constraints.isNotEmpty).toEqual('{\"message\":\"O email do admin deve ser informado!\",\"field\":\"email\"}');
+        expect(errors[2].constraints.isEmail).toEqual('{\"message\":\"O email do admin não é válido!\",\"field\":\"email\"}');
+        expect(errors.length).toEqual(3);
+    });
+});
