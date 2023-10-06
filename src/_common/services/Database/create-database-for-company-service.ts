295d92e4f33e6a327c4ea901ffaddcf75e22e835
Marcos Soares <marcossaore@gmail.com>
2023-10-05 23:52:50 -0300

    chore: remove AdminCompanyService when CreateDatabaseForCompany

--- src/_common/services/Database/create-database-for-company-service.ts
+++ src/_common/services/Database/create-database-for-company-service.ts
@@ -10,15 +10,13 @@ import { EmailService } from "../Email/email.service";
 export class CreateDatabaseForCompanyService {
 
     constructor(
-        private readonly adminCompanyService: AdminCompanyService,
         private readonly companyService: CompanyService,
         private readonly generateDbCredentialsService: GenerateDbCredentialsService,
         private readonly createDataBaseService: CreateDatabaseService,
         private readonly emailService: EmailService
     ) {}
     
-    async create (companyUuid: string, admins: AdminCompany[]): Promise<void> {
-        this.adminCompanyService.createBulk(admins);
+    async create (companyUuid: string): Promise<void> {
         const company = await this.companyService.findByUuid(companyUuid);
         const credentials = this.generateDbCredentialsService.generate();
         this.createDataBaseService.create(credentials);
--- test/_common/services/Database/create-database-for-company-service.spec.ts
+++ test/_common/services/Database/create-database-for-company-service.spec.ts
@@ -1,19 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { mockAdmin, mockAdminEntity, mockCompanyEntity } from '../../../mock/company.mock';
-
+import { mockAdmin, mockCompanyEntity } from '../../../mock/company.mock';
+import { Company } from '../../../../src/company/entities/company.entity';
 import { CompanyService } from '../../../../src/company/company.service';
 import { AdminCompanyService } from '../../../../src/company/admin-company.service';
 import { CreateDatabaseForCompanyService } from '../../../../src/_common/services/Database/create-database-for-company-service';
 import { GenerateDbCredentialsService } from '../../../../src/_common/services/Database/generate-db-credentials.service';
 import { EmailService } from '../../../../src/_common/services/Email/email.service';
 import { CreateDatabaseService } from '../../../../src/_common/services/Database/create-database.service';
-import { Company } from 'src/company/entities/company.entity';
 
 describe('Service: CreateDatabaseForCompanyService', () => {
 
     let companyEntityMock : Company;
     let sutCreateDatabaseForCompanyService: CreateDatabaseForCompanyService;
-    let adminCompanyService: AdminCompanyService;
     let companyService: CompanyService;
     let generateDbCredentialsService: GenerateDbCredentialsService;
     let createDataBaseService: CreateDatabaseService;
@@ -26,12 +24,6 @@ describe('Service: CreateDatabaseForCompanyService', () => {
         const module: TestingModule = await Test.createTestingModule({
         providers: [
             CreateDatabaseForCompanyService,
-            {
-                provide: AdminCompanyService,
-                useValue: {
-                    createBulk: jest.fn().mockResolvedValue(mockAdminEntity())
-                }
-            },
             {
                 provide: CompanyService,
                 useValue: {
@@ -65,7 +57,6 @@ describe('Service: CreateDatabaseForCompanyService', () => {
         .compile();
 
         sutCreateDatabaseForCompanyService = module.get<CreateDatabaseForCompanyService>(CreateDatabaseForCompanyService);
-        adminCompanyService = module.get<AdminCompanyService>(AdminCompanyService);
         companyService = module.get<CompanyService>(CompanyService);
         generateDbCredentialsService = module.get<GenerateDbCredentialsService>(GenerateDbCredentialsService);
         createDataBaseService = module.get<CreateDatabaseService>(CreateDatabaseService);
@@ -77,26 +68,19 @@ describe('Service: CreateDatabaseForCompanyService', () => {
         jest.clearAllMocks();
     });
 
-    it('should call AdminCompanyService.createBulk with correct values', async () => {
-        const admins = [mockAdmin(), mockAdmin()];
-        await sutCreateDatabaseForCompanyService.create('any_uuid', admins);
-        expect(adminCompanyService.createBulk).toHaveBeenCalledTimes(1);
-        expect(adminCompanyService.createBulk).toHaveBeenCalledWith(admins);
-    });
-
     it('should call CompanyService.findByUuid with correct uuid', async () => {
-        await sutCreateDatabaseForCompanyService.create('any_uuid', 'any_admin' as any);
+        await sutCreateDatabaseForCompanyService.create('any_uuid');
         expect(companyService.findByUuid).toHaveBeenCalledTimes(1);
         expect(companyService.findByUuid).toHaveBeenCalledWith('any_uuid');
     });
 
     it('should call GenerateDbCredentialsService.create', async () => {
-        await sutCreateDatabaseForCompanyService.create('any_uuid', 'any_admin' as any);
+        await sutCreateDatabaseForCompanyService.create('any_uuid');
         expect(generateDbCredentialsService.generate).toHaveBeenCalledTimes(1);
     });
 
     it('should call CreateDatabaseService.create', async () => {
-        await sutCreateDatabaseForCompanyService.create('any_uuid', 'any_admin' as any);
+        await sutCreateDatabaseForCompanyService.create('any_uuid');
         expect(createDataBaseService.create).toHaveBeenCalledTimes(1);
         expect(createDataBaseService.create).toHaveBeenCalledWith({
             db: 'any_db', 
@@ -106,7 +90,7 @@ describe('Service: CreateDatabaseForCompanyService', () => {
     });
 
     it('should call EmailService.send with correct values', async () => {
-        await sutCreateDatabaseForCompanyService.create('any_uuid', 'any_admin' as any);
+        await sutCreateDatabaseForCompanyService.create('any_uuid');
         expect(emailService.send).toHaveBeenCalledTimes(1);
         expect(emailService.send).toHaveBeenCalledWith({
             to: companyEntityMock.email,
