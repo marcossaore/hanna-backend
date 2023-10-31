856eac4fbfa82bd8fd5251833eb7a0dd77d55313
Marcos Soares <marcossaore@gmail.com>
2023-10-31 18:48:48 -0300

    fix: change auth login to use auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Request, UseGuards, Get } from '@nestjs/common';
+import { Controller, Request, UseGuards, Post } from '@nestjs/common';
 import { AuthenticatedGuard } from './authenticated.guard';
 
 @Controller('auth')
 export class AuthController {
   @UseGuards(AuthenticatedGuard)
-  @Get('/login')
+  @Post('/login')
   //tipar o retorno
   async login(@Request() req): Promise<any> {
     return req.user;
