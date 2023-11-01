d4da8925f7b2a273d0573cb317070a18d7c86235
Marcos Soares <marcossaore@gmail.com>
2023-11-01 18:46:57 -0300

    feat: add methods in CustomerService

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GenerateUuidService } from '../_common/services/Uuid/generate-uuid-service';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
+import { Customer } from '../../db/companies/entities/customer/customer.entity';
 
 @Module({
-  controllers: [CustomerController],
-  providers: [GenerateUuidService, CustomerService],
+    imports: [
+        TypeOrmModule.forFeature([Customer])
+    ],
+    controllers: [CustomerController],
+    providers: [GenerateUuidService, CustomerService],
 })
 export class CustomerModule {}
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { Customer } from '../../db/companies/entities/customer/customer.entity';
 import { CreateCustomerToEntity } from './dto/create-customer-to-entity.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CustomerService {
 
+    constructor(
+        @InjectRepository(Customer)
+        private readonly companyRepository: Repository<Customer>
+    ) {}
+
     async findByPhone(phone: string): Promise<Customer> {
-        return null;
+        return this.companyRepository.findOneBy({ phone });
     } 
 
     async create(createCustomerDto: CreateCustomerToEntity) {
-        return null;
+        return this.companyRepository.save(createCustomerDto);
     }
 
     // findAll() {
