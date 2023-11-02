9bad75713be8f8844dbe18f797a5c53fc40d6aeb
Marcos Soares <marcossaore@gmail.com>
2023-11-02 18:31:18 -0300

    chore: include customer module

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { GenerateUuidService } from '../_common/services/Uuid/generate-uuid-service';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
-import { Customer } from '../../db/companies/entities/customer/customer.entity';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([Customer])
-    ],
     controllers: [CustomerController],
     providers: [GenerateUuidService, CustomerService],
 })
