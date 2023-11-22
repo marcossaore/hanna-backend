06c99343819829c91e34df4e74cceb710d3d123e
Marcos Soares <marcossaore@gmail.com>
2023-11-22 17:54:27 -0300

    fix: update scripts path

--- scripts/generate-migrations-to-app.sh
+++ scripts/generate-migrations-to-app.sh
@@ -6,4 +6,4 @@ fi
 
 FILENAME="$1"
 
-npx ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli migration:generate ./infra/db/app/migrations/${FILENAME} -d ./db/app/data-source.ts
+npx ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli migration:generate ./infra/db/app/migrations/${FILENAME} -d ./infra/db/app/data-source.ts
