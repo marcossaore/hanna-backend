9ea97708c802a587f817494d99ce64f280c9ba3c
Marcos Soares <marcossaore@gmail.com>
2023-11-29 20:24:23 -0300

    chore: format code

--- test/unit/infra/plugins/token/token-service.adapter.spec.ts
+++ test/unit/infra/plugins/token/token-service.adapter.spec.ts
@@ -5,10 +5,12 @@ describe('Service: TokenServiceAdapter', () => {
     let sutTokenServiceAdapter: TokenServiceAdapter;
 
     beforeEach(async () => {
-        sutTokenServiceAdapter = new TokenServiceAdapter({ get: jest.fn().mockReturnValue({
-            secret: 'any_secret', 
-            expiresIn: 'any_expires_in'
-        })} as any);
+        sutTokenServiceAdapter = new TokenServiceAdapter({
+            get: jest.fn().mockReturnValue({
+                secret: 'any_secret',
+                expiresIn: 'any_expires_in',
+            }),
+        } as any);
         jest.spyOn(jsonwebtoken, 'sign').mockReturnValue('token_value');
         jest.spyOn(jsonwebtoken, 'verify').mockReturnValue('untoken_value');
     });
@@ -20,16 +22,16 @@ describe('Service: TokenServiceAdapter', () => {
     describe('sign', () => {
         it('should call jsonwebtoken.sign with correct key', async () => {
             sutTokenServiceAdapter.sign({
-                key: 'value'
+                key: 'value',
             });
             expect(jsonwebtoken.sign).toBeCalledWith(
                 {
-                    key: 'value'
+                    key: 'value',
                 },
                 'any_secret',
                 {
-                    expiresIn: 'any_expires_in'
-                }
+                    expiresIn: 'any_expires_in',
+                },
             );
             expect(jsonwebtoken.sign).toBeCalledTimes(1);
         });
@@ -38,12 +40,14 @@ describe('Service: TokenServiceAdapter', () => {
             jest.spyOn(jsonwebtoken, 'sign').mockImplementationOnce(() => {
                 throw new Error('JWT ERROR');
             });
-            expect(() => sutTokenServiceAdapter.sign({ key: 'value'})).toThrowError('JWT ERROR');
+            expect(() =>
+                sutTokenServiceAdapter.sign({ key: 'value' }),
+            ).toThrowError('JWT ERROR');
         });
 
         it('should returns a token value on success', async () => {
             const response = sutTokenServiceAdapter.sign({
-                key: 'value'
+                key: 'value',
             });
             expect(response).toBe('token_value');
         });
@@ -52,16 +56,20 @@ describe('Service: TokenServiceAdapter', () => {
     describe('verify', () => {
         it('should call jsonwebtoken.verify with correct values', async () => {
             sutTokenServiceAdapter.verify('any_token');
-            expect(jsonwebtoken.verify).toBeCalledWith('any_token', 'any_secret');
+            expect(jsonwebtoken.verify).toBeCalledWith(
+                'any_token',
+                'any_secret',
+            );
             expect(jsonwebtoken.verify).toBeCalledTimes(1);
         });
-        
 
         it('should return null if jsonwebtoken.verify throws', async () => {
             jest.spyOn(jsonwebtoken, 'verify').mockImplementationOnce(() => {
                 throw new Error('JWT ERROR');
             });
-            expect(() => sutTokenServiceAdapter.verify('any_token')).toThrowError('JWT ERROR');
+            expect(() =>
+                sutTokenServiceAdapter.verify('any_token'),
+            ).toThrowError('JWT ERROR');
         });
 
         it('should an untoken value when succeds', async () => {
