4c28f9ac69ebe9c16eb57a82ad10700b6143d6b5
Marcos Soares <marcossaore@gmail.com>
2023-12-13 13:14:01 -0300

    feat: add CreateProductDto

---
+++ src/modules/application/product/dto/create-product.dto.ts
@@ -0,0 +1,73 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator'
+export class CreateProductDto {
+  @IsString({
+    message: JSON.stringify({
+      message: 'O nome do produto deve ser "string"!',
+      field: 'name'
+    })
+  })
+  @IsNotEmpty({
+    message: JSON.stringify({
+      message: 'O nome do produto deve ser informado!',
+      field: 'name'
+    })
+  })
+  readonly name: string
+
+  @IsString({
+    message: JSON.stringify({
+      message: 'A descrição do produto deve ser "string"!',
+      field: 'description'
+    })
+  })
+  @IsOptional()
+  readonly description: string
+
+  @IsNumber(
+    {
+      allowInfinity: false
+    },
+    {
+      message: JSON.stringify({
+        message: 'O preço do produto deve ser "number"!',
+        field: 'price'
+      })
+    }
+  )
+  @IsNotEmpty({
+    message: JSON.stringify({
+      message: 'O preço do produto deve ser informado!',
+      field: 'price'
+    })
+  })
+  readonly price: number
+
+  @IsNumber(
+    {
+      allowInfinity: false
+    },
+    {
+      message: JSON.stringify({
+        message: 'O preço a granel do produto deve ser "number"!',
+        field: 'bulkPrice'
+      })
+    }
+  )
+  @IsNotEmpty({
+    message: JSON.stringify({
+      message: 'O preço a granel do produto deve ser informado!',
+      field: 'bulkPrice'
+    })
+  })
+  @IsOptional()
+  readonly bulkPrice: number
+
+  @IsString({
+    message: JSON.stringify({
+      message: 'O código de barras do produto deve ser "string"!',
+      field: 'code'
+    })
+  })
+  @IsOptional()
+  readonly code: string
+}
