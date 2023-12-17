ca873edbb6322e425d9e5d633c25082112582870
Marcos Soares <marcossaore@gmail.com>
2023-12-17 10:17:40 -0300

    feat: add sale create flow service

---
+++ src/modules/application/sale/sale.service.ts
@@ -0,0 +1,17 @@
+import { Connection, Repository } from 'typeorm'
+import { Inject, Injectable } from '@nestjs/common'
+import { Sale } from '@infra/db/companies/entities/sale/sale.entity'
+import { CreateSaleDto } from './dto/create-sale.dto'
+
+@Injectable()
+export class SaleService {
+  private readonly saleRepository: Repository<Sale>
+
+  constructor(@Inject('CONNECTION') private readonly connection: Connection) {
+    this.saleRepository = this.connection.getRepository(Sale)
+  }
+
+  create(createSaleDto: CreateSaleDto & { amount: number }) {
+    return this.saleRepository.save(createSaleDto)
+  }
+}
